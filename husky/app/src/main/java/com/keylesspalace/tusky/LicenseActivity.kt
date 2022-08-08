da51a723e611795f0c13bcc90bcacc865c769ee0
Adolfo Santiago <epoch@nixnetmail.com>
2022-08-08 16:21:15 +0200

    LicenseActivity to ViewBinding

--- husky/app/src/main/java/com/keylesspalace/tusky/LicenseActivity.kt
+++ husky/app/src/main/java/com/keylesspalace/tusky/LicenseActivity.kt
@@ -1,42 +1,47 @@
-/* Copyright 2018 Conny Duck
+/*
+ * Husky -- A Pleroma client for Android
  *
- * This file is a part of Tusky.
+ * Copyright (C) 2022  The Husky Developers
+ * Copyright (C) 2018  Conny Duck
  *
- * This program is free software; you can redistribute it and/or modify it under the terms of the
- * GNU General Public License as published by the Free Software Foundation; either version 3 of the
- * License, or (at your option) any later version.
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
- * Tusky is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even
- * the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
- * Public License for more details.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License along with Tusky; if not,
- * see <http://www.gnu.org/licenses>. */
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
 
 package com.keylesspalace.tusky
 
 import android.os.Bundle
-import androidx.annotation.RawRes
-import android.util.Log
 import android.view.MenuItem
 import android.widget.TextView
+import androidx.annotation.RawRes
+import com.keylesspalace.tusky.databinding.ActivityLicenseBinding
 import com.keylesspalace.tusky.util.IOUtils
-import kotlinx.android.extensions.CacheImplementation
-import kotlinx.android.extensions.ContainerOptions
-import kotlinx.android.synthetic.main.activity_license.*
-import kotlinx.android.synthetic.main.toolbar_basic.*
 import java.io.BufferedReader
 import java.io.IOException
 import java.io.InputStreamReader
+import timber.log.Timber
 
 class LicenseActivity : BaseActivity() {
 
-    @ContainerOptions(cache = CacheImplementation.NO_CACHE)
+    private lateinit var binding: ActivityLicenseBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_license)
 
-        setSupportActionBar(toolbar)
+        binding = ActivityLicenseBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.includedToolbar.toolbar)
         supportActionBar?.run {
             setDisplayHomeAsUpEnabled(true)
             setDisplayShowHomeEnabled(true)
@@ -44,41 +49,36 @@ class LicenseActivity : BaseActivity() {
 
         setTitle(R.string.title_licenses)
 
-        loadFileIntoTextView(R.raw.apache, licenseApacheTextView)
-
+        loadFileIntoTextView(R.raw.apache, binding.licenseApacheTextView)
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        when (item.itemId) {
+        when(item.itemId) {
             android.R.id.home -> {
                 onBackPressed()
                 return true
             }
         }
+
         return super.onOptionsItemSelected(item)
     }
 
     private fun loadFileIntoTextView(@RawRes fileId: Int, textView: TextView) {
-
         val sb = StringBuilder()
-
         val br = BufferedReader(InputStreamReader(resources.openRawResource(fileId)))
 
         try {
             var line: String? = br.readLine()
-            while (line != null) {
+            while(line != null) {
                 sb.append(line)
                 sb.append('\n')
                 line = br.readLine()
             }
-        } catch (e: IOException) {
-            Log.w("LicenseActivity", e)
+        } catch(e: IOException) {
+            Timber.w(e)
         }
 
         IOUtils.closeQuietly(br)
-
         textView.text = sb.toString()
-
     }
-
 }
