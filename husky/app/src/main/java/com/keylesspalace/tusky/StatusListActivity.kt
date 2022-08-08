d7fb92f550ae0f5223062368b1bcb8eac91f3a01
Adolfo Santiago <epoch@nixnetmail.com>
2022-08-08 16:28:33 +0200

    StatusListActivity to ViewBinding

--- husky/app/src/main/java/com/keylesspalace/tusky/StatusListActivity.kt
+++ husky/app/src/main/java/com/keylesspalace/tusky/StatusListActivity.kt
@@ -1,17 +1,22 @@
-/* Copyright 2019 Tusky Contributors
+/*
+ * Husky -- A Pleroma client for Android
  *
- * This file is a part of Tusky.
+ * Copyright (C) 2022  The Husky Developers
+ * Copyright (C) 2019  Tusky Contributors
  *
- * This program is free software; you can redistribute it and/or modify it under the terms of the
- * GNU General Public License as published by the Free Software Foundation; either version 3 of the
- * License, or (at your option) any later version.
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
- * Tusky is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even
- * the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
- * Public License for more details.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License along with Tusky; if not,
- * see <https://www.gnu.org/licenses>. */
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
 
 package com.keylesspalace.tusky
 
@@ -20,19 +25,16 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.MenuItem
 import androidx.fragment.app.commit
-
+import com.keylesspalace.tusky.databinding.ActivityStatuslistBinding
 import com.keylesspalace.tusky.fragment.TimelineFragment
 import com.keylesspalace.tusky.fragment.TimelineFragment.Kind
-
-import javax.inject.Inject
-
 import dagger.android.DispatchingAndroidInjector
 import dagger.android.HasAndroidInjector
-import kotlinx.android.extensions.CacheImplementation
-import kotlinx.android.extensions.ContainerOptions
-import kotlinx.android.synthetic.main.toolbar_basic.*
+import javax.inject.Inject
 
 class StatusListActivity : BottomSheetActivity(), HasAndroidInjector {
+
+    private lateinit var binding: ActivityStatuslistBinding
 
     @Inject
     lateinit var dispatchingAndroidInjector: DispatchingAndroidInjector<Any>
@@ -40,12 +42,13 @@ class StatusListActivity : BottomSheetActivity(), HasAndroidInjector {
     private val kind: Kind
         get() = Kind.valueOf(intent.getStringExtra(EXTRA_KIND)!!)
 
-    @ContainerOptions(cache = CacheImplementation.NO_CACHE)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_statuslist)
 
-        setSupportActionBar(toolbar)
+        binding = ActivityStatuslistBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.includedToolbar.toolbar)
 
         val title = if(kind == Kind.FAVOURITES) {
             R.string.title_favourites
@@ -63,14 +66,14 @@ class StatusListActivity : BottomSheetActivity(), HasAndroidInjector {
             val fragment = TimelineFragment.newInstance(kind)
             replace(R.id.fragment_container, fragment)
         }
-
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        if (item.itemId == android.R.id.home){
+        if(item.itemId == android.R.id.home) {
             onBackPressed()
             return true
         }
+
         return super.onOptionsItemSelected(item)
     }
 
@@ -82,15 +85,14 @@ class StatusListActivity : BottomSheetActivity(), HasAndroidInjector {
 
         @JvmStatic
         fun newFavouritesIntent(context: Context) =
-                Intent(context, StatusListActivity::class.java).apply {
-                    putExtra(EXTRA_KIND, Kind.FAVOURITES.name)
-                }
+            Intent(context, StatusListActivity::class.java).apply {
+                putExtra(EXTRA_KIND, Kind.FAVOURITES.name)
+            }
 
         @JvmStatic
         fun newBookmarksIntent(context: Context) =
-                Intent(context, StatusListActivity::class.java).apply {
-                    putExtra(EXTRA_KIND, Kind.BOOKMARKS.name)
-                }
+            Intent(context, StatusListActivity::class.java).apply {
+                putExtra(EXTRA_KIND, Kind.BOOKMARKS.name)
+            }
     }
-
 }
