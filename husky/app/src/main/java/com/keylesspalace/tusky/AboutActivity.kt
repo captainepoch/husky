a4575e2198a9d0198b1b2faa90fec87bfaf585fc
Adolfo Santiago <epoch@nixnetmail.com>
2022-08-24 16:35:28 +0200

    AboutActivity to ViewBinding

--- husky/app/src/main/java/com/keylesspalace/tusky/AboutActivity.kt
+++ husky/app/src/main/java/com/keylesspalace/tusky/AboutActivity.kt
@@ -1,28 +1,49 @@
+/*
+ * Husky -- A Pleroma client for Android
+ *
+ * Copyright (C) 2022  The Husky Developers
+ * Copyright (C) 2018  Conny Duck
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package com.keylesspalace.tusky
 
 import android.content.Intent
 import android.os.Bundle
-import androidx.annotation.StringRes
 import android.text.SpannableString
-import android.text.SpannableStringBuilder
 import android.text.method.LinkMovementMethod
 import android.text.style.URLSpan
 import android.text.util.Linkify
 import android.view.MenuItem
 import android.widget.TextView
+import androidx.annotation.StringRes
+import com.keylesspalace.tusky.core.extensions.gone
+import com.keylesspalace.tusky.core.extensions.viewBinding
+import com.keylesspalace.tusky.databinding.ActivityAboutBinding
 import com.keylesspalace.tusky.di.Injectable
 import com.keylesspalace.tusky.util.CustomURLSpan
-import com.keylesspalace.tusky.util.hide
-import kotlinx.android.synthetic.main.activity_about.*
-import kotlinx.android.synthetic.main.toolbar_basic.*
 
 class AboutActivity : BottomSheetActivity(), Injectable {
 
+    private val binding by viewBinding(ActivityAboutBinding::inflate)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_about)
+        setContentView(binding.root)
 
-        setSupportActionBar(toolbar)
+        setSupportActionBar(binding.includedToolbar.toolbar)
         supportActionBar?.run {
             setDisplayHomeAsUpEnabled(true)
             setDisplayShowHomeEnabled(true)
@@ -30,28 +51,31 @@ class AboutActivity : BottomSheetActivity(), Injectable {
 
         setTitle(R.string.about_title_activity)
 
-        versionTextView.text = getString(R.string.about_app_version, getString(R.string.app_name), BuildConfig.VERSION_NAME)
+        binding.versionTextView.text = getString(
+            R.string.about_app_version,
+            getString(R.string.app_name),
+            BuildConfig.VERSION_NAME
+        )
 
         if(BuildConfig.CUSTOM_INSTANCE.isBlank()) {
-            aboutPoweredByTusky.hide()
+            binding.aboutPoweredByTusky.gone()
         }
 
-        aboutLicenseInfoTextView.setClickableTextWithoutUnderlines(R.string.about_tusky_license)
-        aboutWebsiteInfoTextView.setClickableTextWithoutUnderlines(R.string.about_project_site)
-        aboutBugsFeaturesInfoTextView.setClickableTextWithoutUnderlines(R.string.about_bug_feature_request_site)
+        binding.aboutLicenseInfoTextView.setClickableTextWithoutUnderlines(R.string.about_tusky_license)
+        binding.aboutWebsiteInfoTextView.setClickableTextWithoutUnderlines(R.string.about_project_site)
+        binding.aboutBugsFeaturesInfoTextView.setClickableTextWithoutUnderlines(R.string.about_bug_feature_request_site)
 
-        tuskyProfileButton.setOnClickListener {
+        binding.tuskyProfileButton.setOnClickListener {
             viewUrl(BuildConfig.SUPPORT_ACCOUNT_URL)
         }
 
-        aboutLicensesButton.setOnClickListener {
+        binding.aboutLicensesButton.setOnClickListener {
             startActivityWithSlideInAnimation(Intent(this, LicenseActivity::class.java))
         }
-
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        when (item.itemId) {
+        when(item.itemId) {
             android.R.id.home -> {
                 onBackPressed()
                 return true
@@ -59,17 +83,15 @@ class AboutActivity : BottomSheetActivity(), Injectable {
         }
         return super.onOptionsItemSelected(item)
     }
-
 }
 
 private fun TextView.setClickableTextWithoutUnderlines(@StringRes textId: Int) {
-
     val text = SpannableString(context.getText(textId))
 
     Linkify.addLinks(text, Linkify.WEB_URLS)
 
-    val urlSpans = text.getSpans(0, text.length, URLSpan::class.java)
-    for (span in urlSpans) {
+
+    text.getSpans(0, text.length, URLSpan::class.java).forEach { span ->
         val start = text.getSpanStart(span)
         val end = text.getSpanEnd(span)
         val flags = text.getSpanFlags(span)
@@ -79,9 +101,19 @@ private fun TextView.setClickableTextWithoutUnderlines(@StringRes textId: Int) {
         text.removeSpan(span)
         text.setSpan(customSpan, start, end, flags)
     }
+    /*val urlSpans = text.getSpans(0, text.length, URLSpan::class.java)
+    for (span in urlSpans) {
+        val start = text.getSpanStart(span)
+        val end = text.getSpanEnd(span)
+        val flags = text.getSpanFlags(span)
+
+        val customSpan = object : CustomURLSpan(span.url) {}
+
+        text.removeSpan(span)
+        text.setSpan(customSpan, start, end, flags)
+    }*/
 
     setText(text)
     linksClickable = true
     movementMethod = LinkMovementMethod.getInstance()
-
 }
