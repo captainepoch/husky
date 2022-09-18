fd922493c9710912f7fb7812dfd905c0047d7738
Adolfo Santiago <epoch@nixnetmail.com>
2022-09-18 08:26:31 +0200

    SplashActivity to ViewBinding

--- husky/app/src/main/java/com/keylesspalace/tusky/SplashActivity.kt
+++ husky/app/src/main/java/com/keylesspalace/tusky/SplashActivity.kt
@@ -1,29 +1,36 @@
-/* Copyright 2018 Conny Duck
+/*
+ * Husky -- A Pleroma client for Android
  *
- * This file is a part of Tusky.
+ * Copyright (C) 2022  The Husky Developers
+ * Copyright (C) 2018  Conny Duck
  *
- * This program is free software; you can redistribute it and/or modify it under the terms of the
- * GNU General Public License as published by the Free Software Foundation; either version 3 of the
- * License, or (at your option) any later version.
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
- * Tusky is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even
- * the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
- * Public License for more details.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License along with Tusky; if not,
- * see <http://www.gnu.org/licenses>. */
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
 
 package com.keylesspalace.tusky
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.keylesspalace.tusky.LoginMode.DEFAULT
+import com.keylesspalace.tusky.components.notifications.NotificationHelper
 import com.keylesspalace.tusky.db.AccountManager
 import com.keylesspalace.tusky.di.Injectable
-
-import com.keylesspalace.tusky.components.notifications.NotificationHelper
 import javax.inject.Inject
 
+@SuppressLint("CustomSplashScreen")
 class SplashActivity : AppCompatActivity(), Injectable {
 
     @Inject
@@ -32,19 +39,19 @@ class SplashActivity : AppCompatActivity(), Injectable {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        /** delete old notification channels */
+        // Delete old notification channels
         NotificationHelper.deleteLegacyNotificationChannels(this, accountManager)
 
-        /** Determine whether the user is currently logged in, and if so go ahead and load the
-         *  timeline. Otherwise, start the activity_login screen. */
-
-        val intent = if (accountManager.activeAccount != null) {
+        /**
+         * Determine whether the user is currently logged in, and if so go ahead and load the
+         *  timeline. Otherwise, start the login flow.
+         */
+        val intent = if(accountManager.activeAccount != null) {
             Intent(this, MainActivity::class.java)
         } else {
-            LoginActivity.getIntent(this, false)
+            LoginActivity.getIntent(this, DEFAULT)
         }
         startActivity(intent)
         finish()
     }
-
 }
