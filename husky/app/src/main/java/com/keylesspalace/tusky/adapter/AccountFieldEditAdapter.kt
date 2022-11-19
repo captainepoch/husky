b85255b16f256b050f06020d19e273edc5ccb776
Adolfo Santiago <epoch@nixnetmail.com>
2022-11-19 13:17:19 +0100

    AccountFieldEditAdapter to ViewBinding

--- husky/app/src/main/java/com/keylesspalace/tusky/adapter/AccountFieldEditAdapter.kt
+++ husky/app/src/main/java/com/keylesspalace/tusky/adapter/AccountFieldEditAdapter.kt
@@ -1,30 +1,33 @@
-/* Copyright 2018 Conny Duck
+/*
+ * Husky -- A Pleroma client for Android
  *
- * This file is a part of Tusky.
+ * Copyright (C) 2022  The Husky Developers
+ * Copyright (C) 2018  Conny Duck
  *
- * This program is free software; you can redistribute it and/or modify it under the terms of the
- * GNU General Public License as published by the Free Software Foundation; either version 3 of the
- * License, or (at your option) any later version.
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
- * Tusky is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even
- * the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
- * Public License for more details.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License along with Tusky; if not,
- * see <http://www.gnu.org/licenses>. */
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
 
 package com.keylesspalace.tusky.adapter
 
 import android.text.Editable
 import android.text.TextWatcher
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
 import android.widget.EditText
 import androidx.recyclerview.widget.RecyclerView
-import com.keylesspalace.tusky.R
+import com.keylesspalace.tusky.databinding.ItemEditFieldBinding
 import com.keylesspalace.tusky.entity.StringField
-import kotlinx.android.synthetic.main.item_edit_field.view.*
 
 class AccountFieldEditAdapter : RecyclerView.Adapter<AccountFieldEditAdapter.ViewHolder>() {
 
@@ -36,6 +39,7 @@ class AccountFieldEditAdapter : RecyclerView.Adapter<AccountFieldEditAdapter.Vie
         fields.forEach { field ->
             fieldData.add(MutableStringPair(field.name, field.value))
         }
+
         if (fieldData.isEmpty()) {
             fieldData.add(MutableStringPair("", ""))
         }
@@ -57,7 +61,9 @@ class AccountFieldEditAdapter : RecyclerView.Adapter<AccountFieldEditAdapter.Vie
     override fun getItemCount(): Int = fieldData.size
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_edit_field, parent, false)
+        val view = ItemEditFieldBinding.inflate(
+            LayoutInflater.from(parent.context), parent, false
+        )
         return ViewHolder(view)
     }
 
@@ -67,7 +73,7 @@ class AccountFieldEditAdapter : RecyclerView.Adapter<AccountFieldEditAdapter.Vie
 
         viewHolder.nameTextView.addTextChangedListener(object : TextWatcher {
             override fun afterTextChanged(newText: Editable) {
-                fieldData[viewHolder.adapterPosition].first = newText.toString()
+                fieldData[viewHolder.absoluteAdapterPosition].first = newText.toString()
             }
 
             override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {}
@@ -77,7 +83,7 @@ class AccountFieldEditAdapter : RecyclerView.Adapter<AccountFieldEditAdapter.Vie
 
         viewHolder.valueTextView.addTextChangedListener(object : TextWatcher {
             override fun afterTextChanged(newText: Editable) {
-                fieldData[viewHolder.adapterPosition].second = newText.toString()
+                fieldData[viewHolder.absoluteAdapterPosition].second = newText.toString()
             }
 
             override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {}
@@ -86,9 +92,9 @@ class AccountFieldEditAdapter : RecyclerView.Adapter<AccountFieldEditAdapter.Vie
         })
     }
 
-    class ViewHolder(rootView: View) : RecyclerView.ViewHolder(rootView) {
-        val nameTextView: EditText = rootView.accountFieldName
-        val valueTextView: EditText = rootView.accountFieldValue
+    class ViewHolder(view: ItemEditFieldBinding) : RecyclerView.ViewHolder(view.root) {
+        val nameTextView: EditText = view.accountFieldName
+        val valueTextView: EditText = view.accountFieldValue
     }
 
     class MutableStringPair(var first: String, var second: String)
