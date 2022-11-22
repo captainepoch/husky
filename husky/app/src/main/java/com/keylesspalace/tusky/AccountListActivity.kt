c939d0a4aefc945d3e27fc694027b613f4c3a417
Adolfo Santiago <epoch@nixnetmail.com>
2022-11-22 21:56:43 +0100

    AccountListActivity to ViewBinding

--- husky/app/src/main/java/com/keylesspalace/tusky/AccountListActivity.kt
+++ husky/app/src/main/java/com/keylesspalace/tusky/AccountListActivity.kt
@@ -1,17 +1,22 @@
-/* Copyright 2017 Andrew Dawson
+/*
+ * Husky -- A Pleroma client for Android
  *
- * This file is a part of Tusky.
+ * Copyright (C) 2022  The Husky Developers
+ * Copyright (C) 2017  Andrew Dawson
  *
- * This program is free software; you can redistribute it and/or modify it under the terms of the
- * GNU General Public License as published by the Free Software Foundation; either version 3 of the
- * License, or (at your option) any later version.
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
- * Tusky is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even
- * the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
- * Public License for more details.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License along with Tusky; if not,
- * see <http://www.gnu.org/licenses>. */
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
 
 package com.keylesspalace.tusky
 
@@ -19,13 +24,16 @@ import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.view.MenuItem
+import com.keylesspalace.tusky.core.extensions.viewBinding
+import com.keylesspalace.tusky.databinding.ActivityAccountListBinding
 import com.keylesspalace.tusky.fragment.AccountListFragment
 import dagger.android.DispatchingAndroidInjector
 import dagger.android.HasAndroidInjector
-import kotlinx.android.synthetic.main.toolbar_basic.*
 import javax.inject.Inject
 
 class AccountListActivity : BaseActivity(), HasAndroidInjector {
+
+    private val binding by viewBinding(ActivityAccountListBinding::inflate)
 
     @Inject
     lateinit var dispatchingAndroidInjector: DispatchingAndroidInjector<Any>
@@ -43,13 +51,13 @@ class AccountListActivity : BaseActivity(), HasAndroidInjector {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_account_list)
+        setContentView(binding.root)
 
         val type = intent.getSerializableExtra(EXTRA_TYPE) as Type
         val id: String? = intent.getStringExtra(EXTRA_ID)
         val emoji: String? = intent.getStringExtra(EXTRA_EMOJI)
 
-        setSupportActionBar(toolbar)
+        setSupportActionBar(binding.includedToolbar.toolbar)
         supportActionBar?.apply {
             when (type) {
                 Type.BLOCKS -> setTitle(R.string.title_blocks)
@@ -59,7 +67,12 @@ class AccountListActivity : BaseActivity(), HasAndroidInjector {
                 Type.FOLLOWS -> setTitle(R.string.title_follows)
                 Type.REBLOGGED -> setTitle(R.string.title_reblogged_by)
                 Type.FAVOURITED -> setTitle(R.string.title_favourited_by)
-                Type.REACTED -> setTitle(String.format(getString(R.string.title_emoji_reacted_by), emoji))
+                Type.REACTED -> setTitle(
+                    String.format(
+                        getString(R.string.title_emoji_reacted_by),
+                        emoji
+                    )
+                )
             }
             setDisplayHomeAsUpEnabled(true)
             setDisplayShowHomeEnabled(true)
