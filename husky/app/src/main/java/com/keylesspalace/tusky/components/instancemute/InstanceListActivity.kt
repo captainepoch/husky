963c357806e20c83b3a5d0fb93efd8f50bc26c0e
Adolfo Santiago <epoch@nixnetmail.com>
2023-01-14 09:17:48 +0100

    InstanceListActivity to ViewBinding

--- husky/app/src/main/java/com/keylesspalace/tusky/components/instancemute/InstanceListActivity.kt
+++ husky/app/src/main/java/com/keylesspalace/tusky/components/instancemute/InstanceListActivity.kt
@@ -5,15 +5,18 @@ import android.view.MenuItem
 import com.keylesspalace.tusky.BaseActivity
 import com.keylesspalace.tusky.R
 import com.keylesspalace.tusky.components.instancemute.fragment.InstanceListFragment
-import kotlinx.android.synthetic.main.toolbar_basic.toolbar
+import com.keylesspalace.tusky.core.extensions.viewBinding
+import com.keylesspalace.tusky.databinding.ActivityAccountListBinding
 
 class InstanceListActivity : BaseActivity() {
 
+    private val binding by viewBinding(ActivityAccountListBinding::inflate)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_account_list)
+        setContentView(binding.root)
 
-        setSupportActionBar(toolbar)
+        setSupportActionBar(binding.includedToolbar.toolbar)
         supportActionBar?.apply {
             setTitle(R.string.title_domain_mutes)
             setDisplayHomeAsUpEnabled(true)
