c8b3294ebe579a0a946d54c3fc0d4cf6dee66a35
Adolfo Santiago <epoch@nixnetmail.com>
2023-01-24 19:45:29 +0100

    Fix formatting

--- husky/app/src/main/java/com/keylesspalace/tusky/components/instance/InstancePleromaMetadata.kt
+++ husky/app/src/main/java/com/keylesspalace/tusky/components/instance/InstancePleromaMetadata.kt
@@ -6,5 +6,5 @@ data class InstancePleromaMetadata(
     val features: List<String>,
     @SerializedName("fields_limits") val fieldsLimits: InstancePleromaMetadataFieldsLimits
     // TODO(InstanceCapabilities)
-    //@SerializedName("post_formats") val postsFormats: List<String>
+    // @SerializedName("post_formats") val postsFormats: List<String>
 )
