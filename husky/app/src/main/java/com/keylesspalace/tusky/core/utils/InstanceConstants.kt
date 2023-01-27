8ab994222d269b0a08d29d97c1f9138121259785
Adolfo Santiago <epoch@nixnetmail.com>
2023-01-27 07:32:41 +0100

    Change default bio max. length

--- husky/app/src/main/java/com/keylesspalace/tusky/core/utils/InstanceConstants.kt
+++ husky/app/src/main/java/com/keylesspalace/tusky/core/utils/InstanceConstants.kt
@@ -2,6 +2,6 @@ package com.keylesspalace.tusky.core.utils
 
 object InstanceConstants {
 
-    const val DEFAULT_BIO_LENGTH = 0
+    const val DEFAULT_BIO_LENGTH = -1
     const val DEFAULT_CHARACTER_LIMIT = 500
 }
