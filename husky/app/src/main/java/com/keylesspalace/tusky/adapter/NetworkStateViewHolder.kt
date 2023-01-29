c85bf1e498042f690c4710aaec9b48e4d2c237ec
Adolfo Santiago <epoch@nixnetmail.com>
2023-01-29 15:23:53 +0100

    NetworkStateViewHolder to ViewBinding

--- husky/app/src/main/java/com/keylesspalace/tusky/adapter/NetworkStateViewHolder.kt
+++ husky/app/src/main/java/com/keylesspalace/tusky/adapter/NetworkStateViewHolder.kt
@@ -1,45 +1,49 @@
-/* Copyright 2019 Conny Duck
+/*
+ * Husky -- A Pleroma client for Android
  *
- * This file is a part of Tusky.
+ * Copyright (C) 2023  The Husky Developers
+ * Copyright (C) 2019  Conny Duck
  *
- * This program is free software; you can redistribute it and/or modify it under the terms of the
- * GNU General Public License as published by the Free Software Foundation; either version 3 of the
- * License, or (at your option) any later version.
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
- * Tusky is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even
- * the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
- * Public License for more details.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License along with Tusky; if not,
- * see <http://www.gnu.org/licenses>. */
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
 
 package com.keylesspalace.tusky.adapter
 
-import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.keylesspalace.tusky.databinding.ItemNetworkStateBinding
 import com.keylesspalace.tusky.util.NetworkState
 import com.keylesspalace.tusky.util.Status
 import com.keylesspalace.tusky.util.visible
-import kotlinx.android.synthetic.main.item_network_state.view.*
 
 class NetworkStateViewHolder(
-    itemView: View,
+    private val binding: ItemNetworkStateBinding,
     private val retryCallback: () -> Unit
-) : RecyclerView.ViewHolder(itemView) {
+) : RecyclerView.ViewHolder(binding.root) {
 
     fun setUpWithNetworkState(state: NetworkState?, fullScreen: Boolean) {
-        itemView.progressBar.visible(state?.status == Status.RUNNING)
-        itemView.retryButton.visible(state?.status == Status.FAILED)
-        itemView.errorMsg.visible(state?.msg != null)
-        itemView.errorMsg.text = state?.msg
-        itemView.retryButton.setOnClickListener {
+        binding.progressBar.visible(state?.status == Status.RUNNING)
+        binding.retryButton.visible(state?.status == Status.FAILED)
+        binding.errorMsg.visible(state?.msg != null)
+        binding.errorMsg.text = state?.msg
+        binding.retryButton.setOnClickListener {
             retryCallback()
         }
-        if (fullScreen) {
-            itemView.layoutParams.height = ViewGroup.LayoutParams.MATCH_PARENT
+        if(fullScreen) {
+            binding.root.layoutParams.height = ViewGroup.LayoutParams.MATCH_PARENT
         } else {
-            itemView.layoutParams.height = ViewGroup.LayoutParams.WRAP_CONTENT
+            binding.root.layoutParams.height = ViewGroup.LayoutParams.WRAP_CONTENT
         }
     }
 }
