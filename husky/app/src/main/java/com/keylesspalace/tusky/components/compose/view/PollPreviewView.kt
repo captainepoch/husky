39cf9d353e0c302960e30064468099baf6c8a977
Adolfo Santiago <epoch@nixnetmail.com>
2023-01-29 15:44:51 +0100

    PollPreviewView to ViewBinding

--- husky/app/src/main/java/com/keylesspalace/tusky/components/compose/view/PollPreviewView.kt
+++ husky/app/src/main/java/com/keylesspalace/tusky/components/compose/view/PollPreviewView.kt
@@ -1,49 +1,53 @@
-/* Copyright 2019 Tusky Contributors
+/*
+ * Husky -- A Pleroma client for Android
  *
- * This file is a part of Tusky.
+ * Copyright (C) 2022  The Husky Developers
+ * Copyright (C) 2019  Tusky Contributors
  *
- * This program is free software; you can redistribute it and/or modify it under the terms of the
- * GNU General Public License as published by the Free Software Foundation; either version 3 of the
- * License, or (at your option) any later version.
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
- * Tusky is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even
- * the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
- * Public License for more details.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License along with Tusky; if not,
- * see <http://www.gnu.org/licenses>. */
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
 
 package com.keylesspalace.tusky.components.compose.view
 
 import android.content.Context
 import android.util.AttributeSet
+import android.view.LayoutInflater
 import android.widget.LinearLayout
 import com.keylesspalace.tusky.R
 import com.keylesspalace.tusky.adapter.PreviewPollOptionsAdapter
+import com.keylesspalace.tusky.databinding.ViewPollPreviewBinding
 import com.keylesspalace.tusky.entity.NewPoll
-import kotlinx.android.synthetic.main.view_poll_preview.view.*
 
 class PollPreviewView @JvmOverloads constructor(
-    context: Context?,
+    context: Context,
     attrs: AttributeSet? = null,
     defStyleAttr: Int = 0
-) :
-    LinearLayout(context, attrs, defStyleAttr) {
+) : LinearLayout(context, attrs, defStyleAttr) {
 
-    val adapter = PreviewPollOptionsAdapter()
+    private val binding: ViewPollPreviewBinding
+    private val adapter = PreviewPollOptionsAdapter()
 
     init {
-        inflate(context, R.layout.view_poll_preview, this)
+        binding = ViewPollPreviewBinding.inflate(LayoutInflater.from(context), this)
 
         orientation = VERTICAL
-
         setBackgroundResource(R.drawable.card_frame)
 
         val padding = resources.getDimensionPixelSize(R.dimen.poll_preview_padding)
-
         setPadding(padding, padding, padding, padding)
 
-        pollPreviewOptions.adapter = adapter
+        binding.pollPreviewOptions.adapter = adapter
     }
 
     fun setPoll(poll: NewPoll) {
@@ -52,7 +56,9 @@ class PollPreviewView @JvmOverloads constructor(
         val pollDurationId = resources.getIntArray(R.array.poll_duration_values).indexOfLast {
             it <= poll.expiresIn
         }
-        pollDurationPreview.text = resources.getStringArray(R.array.poll_duration_names)[pollDurationId]
+
+        binding.pollDurationPreview.text =
+            resources.getStringArray(R.array.poll_duration_names)[pollDurationId]
     }
 
     override fun setOnClickListener(l: OnClickListener?) {
