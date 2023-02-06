2a762c3c1333d581f46034c43e5408ce7c1652f1
Adolfo Santiago <epoch@nixnetmail.com>
2023-02-06 20:43:53 +0100

    Update N profile fields'
    Now the profile fields can be updated using the data retreived from the
    instance endpoint to know the max. size of fields allowed.

--- husky/app/src/main/java/com/keylesspalace/tusky/viewmodel/EditProfileViewModel.kt
+++ husky/app/src/main/java/com/keylesspalace/tusky/viewmodel/EditProfileViewModel.kt
@@ -79,7 +79,10 @@ class EditProfileViewModel(
     val profileData = MutableLiveData<Resource<Account>>()
     val avatarData = MutableLiveData<Resource<Bitmap>>()
     val headerData = MutableLiveData<Resource<Bitmap>>()
-    val saveData = MutableLiveData<Resource<Nothing>>()
+
+    private val _saveData: MutableLiveData<Resource<Nothing>> = MutableLiveData<Resource<Nothing>>()
+    val saveData
+        get() = _saveData
 
     private val _instanceData = MutableLiveData<InstanceInfo>()
     val instanceData: LiveData<InstanceInfo>
@@ -169,7 +172,7 @@ class EditProfileViewModel(
         newFields: List<StringField>,
         context: Context
     ) {
-        if (saveData.value is Loading || profileData.value !is Success) {
+        if (_saveData.value is Loading || profileData.value !is Success) {
             return
         }
 
@@ -215,26 +218,26 @@ class EditProfileViewModel(
             null
         }
 
-        // when one field changed, all have to be sent or they unchanged ones would get overridden
-        val fieldsUnchanged = oldProfileData?.source?.fields == newFields
-        val field1 = calculateFieldToUpdate(newFields.getOrNull(0), fieldsUnchanged)
-        val field2 = calculateFieldToUpdate(newFields.getOrNull(1), fieldsUnchanged)
-        val field3 = calculateFieldToUpdate(newFields.getOrNull(2), fieldsUnchanged)
-        val field4 = calculateFieldToUpdate(newFields.getOrNull(3), fieldsUnchanged)
+        val cleanFieldsList = newFields.mapNotNull { value ->
+            value.takeIf { it.name.isNotEmpty() || it.value.isNotEmpty() }
+        }
 
         if (displayName == null &&
             note == null &&
             locked == null &&
             avatar == null &&
             header == null &&
-            field1 == null &&
-            field2 == null &&
-            field3 == null &&
-            field4 == null
+            (oldProfileData?.source?.fields == cleanFieldsList)
         ) {
             // If nothing has changed, there is no need to make a network request
-            saveData.postValue(Success())
+            _saveData.postValue(Success())
             return
+        }
+
+        val fieldsMap = hashMapOf<String, RequestBody>()
+        cleanFieldsList.forEachIndexed { index, stringField ->
+            fieldsMap["fields_attributes[$index][name]"] = stringField.name.toRequestBody()
+            fieldsMap["fields_attributes[$index][value]"] = stringField.value.toRequestBody()
         }
 
         mastodonApi.accountUpdateCredentials(
@@ -243,14 +246,7 @@ class EditProfileViewModel(
             locked,
             avatar,
             header,
-            field1?.first,
-            field1?.second,
-            field2?.first,
-            field2?.second,
-            field3?.first,
-            field3?.second,
-            field4?.first,
-            field4?.second
+            fieldsMap
         ).enqueue(object : Callback<Account> {
 
             override fun onResponse(call: Call<Account>, response: Response<Account>) {
@@ -266,15 +262,15 @@ class EditProfileViewModel(
                     } else {
                         null
                     }
-                    saveData.postValue(Error(errorMessage = errorMsg))
+                    _saveData.postValue(Error(errorMessage = errorMsg))
                     return
                 }
-                saveData.postValue(Success())
+                _saveData.postValue(Success())
                 eventHub.dispatch(ProfileEditedEvent(newProfileData))
             }
 
             override fun onFailure(call: Call<Account>, t: Throwable) {
-                saveData.postValue(Error())
+                _saveData.postValue(Error())
             }
         })
     }
@@ -297,19 +293,6 @@ class EditProfileViewModel(
 
             profileData.postValue(Success(newProfile))
         }
-    }
-
-    private fun calculateFieldToUpdate(
-        newField: StringField?,
-        fieldsUnchanged: Boolean
-    ): Pair<RequestBody, RequestBody>? {
-        if (fieldsUnchanged || newField == null) {
-            return null
-        }
-        return Pair(
-            newField.name.toRequestBody(MultipartBody.FORM),
-            newField.value.toRequestBody(MultipartBody.FORM)
-        )
     }
 
     private fun getCacheFileForName(context: Context, filename: String): File {
