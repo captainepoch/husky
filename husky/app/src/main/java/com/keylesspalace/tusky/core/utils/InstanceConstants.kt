574416da5b2165bef07ca37bb696da1687ce158b
Adolfo Santiago <epoch@nixnetmail.com>
2023-02-06 20:42:26 +0100

    Add max. bio fields size

--- husky/app/src/main/java/com/keylesspalace/tusky/core/utils/InstanceConstants.kt
+++ husky/app/src/main/java/com/keylesspalace/tusky/core/utils/InstanceConstants.kt
@@ -3,5 +3,7 @@ package com.keylesspalace.tusky.core.utils
 object InstanceConstants {
 
     const val DEFAULT_BIO_LENGTH = -1
+    const val DEFAULT_BIO_MAX_FIELDS = 4
+
     const val DEFAULT_CHARACTER_LIMIT = 500
 }
