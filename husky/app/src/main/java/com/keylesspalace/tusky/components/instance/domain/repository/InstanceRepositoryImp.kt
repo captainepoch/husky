ddd938026fd86a55e1f36cb84ed55a82e4ebd88a
Adolfo Santiago <epoch@nixnetmail.com>
2023-05-13 07:56:50 +0200

    Define the repository with an interface

--- husky/app/src/main/java/com/keylesspalace/tusky/components/instance/domain/repository/InstanceRepositoryImp.kt
+++ husky/app/src/main/java/com/keylesspalace/tusky/components/instance/domain/repository/InstanceRepositoryImp.kt
@@ -1,5 +1,25 @@
-package com.keylesspalace.tusky.components.instance
+/*
+ * Husky -- A Pleroma client for Android
+ *
+ * Copyright (C) 2023  The Husky Developers
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
 
+package com.keylesspalace.tusky.components.instance.domain.repository
+
+import com.keylesspalace.tusky.components.instance.data.models.entity.InstanceEntity
 import com.keylesspalace.tusky.core.extensions.notNull
 import com.keylesspalace.tusky.core.functional.Either
 import com.keylesspalace.tusky.db.AccountManager
@@ -8,13 +28,13 @@ import com.keylesspalace.tusky.network.MastodonService
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 
-class InstanceRepository(
+class InstanceRepositoryImp(
     private val accountManager: AccountManager,
     private val service: MastodonService,
     private val db: AppDatabase
-) {
+) : InstanceRepository {
 
-    fun getInstanceInfo(): Flow<Either<Nothing, InstanceEntity>> = flow {
+    override fun getInstanceInfo(): Flow<Either<Nothing, InstanceEntity>> = flow {
         service.getInstanceData().run {
             val response = if (isSuccessful && body().notNull()) {
                 val instance = body()!!
@@ -37,7 +57,7 @@ class InstanceRepository(
         }
     }
 
-    fun getInstanceInfoDb(): InstanceEntity {
+    override fun getInstanceInfoDb(): InstanceEntity {
         return db.instanceDao().loadFromCache(accountManager.activeAccount!!.domain)
     }
 }
