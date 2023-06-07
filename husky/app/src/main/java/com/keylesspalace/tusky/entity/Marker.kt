f495674cb545a20260c91d1c2148bf2342fad769
Adolfo Santiago <epoch@adol.pw>
2023-06-07 18:16:52 +0200

    Update Marker class with custom adapter type

--- husky/app/src/main/java/com/keylesspalace/tusky/entity/Marker.kt
+++ husky/app/src/main/java/com/keylesspalace/tusky/entity/Marker.kt
@@ -1,15 +1,16 @@
 package com.keylesspalace.tusky.entity
 
+import com.google.gson.annotations.JsonAdapter
 import com.google.gson.annotations.SerializedName
+import com.keylesspalace.tusky.json.CustomDateTypeAdapter
 import java.util.Date
 
 /**
  * API type for saving the scroll position of a timeline.
  */
 data class Marker(
-    @SerializedName("last_read_id")
-    val lastReadId: String,
-    val version: Int,
-    @SerializedName("updated_at")
-    val updatedAt: Date
+    @SerializedName("last_read_id") val lastReadId: String,
+    @SerializedName("version") val version: Int,
+    @JsonAdapter(CustomDateTypeAdapter::class)
+    @SerializedName("updated_at") val updatedAt: Date
 )
