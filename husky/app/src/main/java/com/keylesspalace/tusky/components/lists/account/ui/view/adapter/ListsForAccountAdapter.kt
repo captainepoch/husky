80211f214010baf9fd9d37e546074e05f46d8022
Adolfo Santiago <epoch@adol.pw>
2023-10-15 08:36:38 +0200

    Create adapter for the list of lists

---
+++ husky/app/src/main/java/com/keylesspalace/tusky/components/lists/account/ui/view/adapter/ListsForAccountAdapter.kt
@@ -0,0 +1,53 @@
+package com.keylesspalace.tusky.components.lists.account.ui.view.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.keylesspalace.tusky.components.lists.account.model.ListForAccount
+import com.keylesspalace.tusky.databinding.ItemListsForAccountBinding
+
+class ListsForAccountAdapter : ListAdapter<ListForAccount, ListsForAccountAdapter.ViewHolder>(
+    ListDiffer
+) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(
+            ItemListsForAccountBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            )
+        )
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(getItem(position))
+    }
+
+    inner class ViewHolder(
+        private val binding: ItemListsForAccountBinding
+    ) : RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(item: ListForAccount) {
+            binding.listName.text = item.list.title
+        }
+    }
+
+    private object ListDiffer : DiffUtil.ItemCallback<ListForAccount>() {
+        override fun areItemsTheSame(
+            oldItem: ListForAccount,
+            newItem: ListForAccount
+        ): Boolean {
+            return oldItem.list.id == newItem.list.id
+        }
+
+        override fun areContentsTheSame(
+            oldItem: ListForAccount,
+            newItem: ListForAccount
+        ): Boolean {
+            return oldItem == newItem
+        }
+    }
+}
