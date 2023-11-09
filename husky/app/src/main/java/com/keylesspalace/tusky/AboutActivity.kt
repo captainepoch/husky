eb0adbaa86a2b9eeca6b3e41af6ca7c79f2f9d3f
Adolfo Santiago <epoch@adol.pw>
2023-11-09 08:53:37 +0100

    Change deprecated method for callback

--- husky/app/src/main/java/com/keylesspalace/tusky/AboutActivity.kt
+++ husky/app/src/main/java/com/keylesspalace/tusky/AboutActivity.kt
@@ -70,7 +70,8 @@ class AboutActivity : BottomSheetActivity() {
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         when (item.itemId) {
             android.R.id.home -> {
-                onBackPressed()
+                onBackPressedDispatcher.onBackPressed()
+
                 return true
             }
         }
