5c6ecc3dfecbbb8d488dc8fccbcda4973ab0dd4e
Adolfo Santiago <epoch@adol.pw>
2023-11-26 07:12:16 +0100

    Add new field for feature configuration

--- husky/app/src/main/java/com/keylesspalace/tusky/components/instance/data/models/data/InstanceInfo.kt
+++ husky/app/src/main/java/com/keylesspalace/tusky/components/instance/data/models/data/InstanceInfo.kt
@@ -6,5 +6,6 @@ data class InstanceInfo(
     var isLoadingInfo: Boolean = true,
     val maxTootLength: Int = InstanceConstants.DEFAULT_CHARACTER_LIMIT,
     val maxBioLength: Int = InstanceConstants.DEFAULT_BIO_LENGTH,
-    val maxBioFields: Int = InstanceConstants.DEFAULT_BIO_MAX_FIELDS
+    val maxBioFields: Int = InstanceConstants.DEFAULT_BIO_MAX_FIELDS,
+    val quotePosting: Boolean = false
 )
