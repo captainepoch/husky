9e3abb6322622414d15c07323ebffd92fa34f542
Adolfo Santiago <epoch@adol.pw>
2024-04-04 10:00:43 +0200

    Add quote_id to NewStatus

--- husky/app/src/main/java/com/keylesspalace/tusky/entity/NewStatus.kt
+++ husky/app/src/main/java/com/keylesspalace/tusky/entity/NewStatus.kt
@@ -1,41 +1,45 @@
-/* Copyright 2019 Tusky Contributors
+/*
+ * Husky -- A Pleroma client for Android
  *
- * This file is a part of Tusky.
+ * Copyright (C) 2021  The Husky Developers
+ * Copyright (C) 2019  Tusky Contributors
  *
- * This program is free software; you can redistribute it and/or modify it under the terms of the
- * GNU General Public License as published by the Free Software Foundation; either version 3 of the
- * License, or (at your option) any later version.
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
- * Tusky is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even
- * the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
- * Public License for more details.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License along with Tusky; if not,
- * see <http://www.gnu.org/licenses>. */
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
 
 package com.keylesspalace.tusky.entity
 
-import android.os.Parcelable
 import com.google.gson.annotations.SerializedName
-import kotlinx.android.parcel.Parcelize
 
 data class NewStatus(
     val status: String,
-    @SerializedName("spoiler_text") val warningText: String,
-    @SerializedName("in_reply_to_id") val inReplyToId: String?,
+    @SerializedName("spoiler_text")
+    val warningText: String,
+    @SerializedName("in_reply_to_id")
+    val inReplyToId: String?,
     val visibility: String,
     val sensitive: Boolean,
-    @SerializedName("media_ids") val mediaIds: List<String>?,
-    @SerializedName("scheduled_at") val scheduledAt: String?,
-    @SerializedName("expires_in") val expiresIn: Int?,
+    @SerializedName("media_ids")
+    val mediaIds: List<String>?,
+    @SerializedName("scheduled_at")
+    val scheduledAt: String?,
+    @SerializedName("expires_in")
+    val expiresIn: Int?,
     val poll: NewPoll?,
-    @SerializedName("content_type") var contentType: String?,
-    val preview: Boolean?
+    @SerializedName("content_type")
+    var contentType: String?,
+    val preview: Boolean?,
+    @SerializedName("quote_id")
+    val quoteId: String?
 )
-
-@Parcelize
-data class NewPoll(
-    val options: List<String>,
-    @SerializedName("expires_in") val expiresIn: Int,
-    val multiple: Boolean
-) : Parcelable
