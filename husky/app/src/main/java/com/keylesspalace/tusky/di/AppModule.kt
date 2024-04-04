3a942f9aa252909948259d8fdbdafb870aa0e8c9
Adolfo Santiago <epoch@adol.pw>
2024-04-04 09:09:09 +0200

    Migrate DB to 31 for the new value

--- husky/app/src/main/java/com/keylesspalace/tusky/di/AppModule.kt
+++ husky/app/src/main/java/com/keylesspalace/tusky/di/AppModule.kt
@@ -94,7 +94,8 @@ val appModule = module {
                 AppDatabase.MIGRATION_27_28,
                 AppDatabase.MIGRATION_28_29,
                 AppDatabase.MIGRATION_29_30,
-                AppDatabase.MIGRATION_30_31
+                AppDatabase.MIGRATION_30_31,
+                AppDatabase.MIGRATION_31_32
             ).build()
     } bind AppDatabase::class
 
