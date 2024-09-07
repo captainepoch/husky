406b8086e6023f8176edd22857e8734791d3fda7
Adolfo Santiago <epoch@adol.pw>
2024-09-07 13:04:25 +0200

    Use InstanceRepository

--- husky/app/src/main/java/com/keylesspalace/tusky/components/announcements/AnnouncementsViewModel.kt
+++ husky/app/src/main/java/com/keylesspalace/tusky/components/announcements/AnnouncementsViewModel.kt
@@ -1,31 +1,32 @@
-/* Copyright 2020 Tusky Contributors
+/*
+ * Husky -- A Pleroma client for Android
  *
- * This file is a part of Tusky.
+ * Copyright (C) 2024  The Husky Developers
+ * Copyright (C) 2020  Tusky Contributors
  *
- * This program is free software; you can redistribute it and/or modify it under the terms of the
- * GNU General Public License as published by the Free Software Foundation; either version 3 of the
- * License, or (at your option) any later version.
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
- * Tusky is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even
- * the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
- * Public License for more details.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License along with Tusky; if not,
- * see <http://www.gnu.org/licenses>. */
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
 
 package com.keylesspalace.tusky.components.announcements
 
-import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
 import com.keylesspalace.tusky.appstore.AnnouncementReadEvent
 import com.keylesspalace.tusky.appstore.EventHub
-import com.keylesspalace.tusky.components.instance.data.models.data.Instance
-import com.keylesspalace.tusky.components.instance.data.models.entity.InstanceEntity
-import com.keylesspalace.tusky.core.functional.Either
-import com.keylesspalace.tusky.core.utils.InstanceConstants
-import com.keylesspalace.tusky.db.AccountManager
-import com.keylesspalace.tusky.db.AppDatabase
+import com.keylesspalace.tusky.components.instance.domain.repository.InstanceRepository
+import com.keylesspalace.tusky.core.extensions.cancelIfActive
 import com.keylesspalace.tusky.entity.Announcement
 import com.keylesspalace.tusky.entity.Emoji
 import com.keylesspalace.tusky.network.MastodonApi
@@ -34,60 +35,36 @@ import com.keylesspalace.tusky.util.Loading
 import com.keylesspalace.tusky.util.Resource
 import com.keylesspalace.tusky.util.RxAwareViewModel
 import com.keylesspalace.tusky.util.Success
-import io.reactivex.rxkotlin.Singles
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
+import timber.log.Timber
 
 class AnnouncementsViewModel(
-    accountManager: AccountManager,
-    private val appDatabase: AppDatabase,
     private val mastodonApi: MastodonApi,
+    private val instanceRepository: InstanceRepository,
     private val eventHub: EventHub
 ) : RxAwareViewModel() {
 
+    private var job: Job? = null
+
     private val announcementsMutable = MutableLiveData<Resource<List<Announcement>>>()
-    val announcements: LiveData<Resource<List<Announcement>>> = announcementsMutable
+    val announcements: LiveData<Resource<List<Announcement>>>
+        get() = announcementsMutable
 
     private val emojisMutable = MutableLiveData<List<Emoji>?>()
     val emojis: LiveData<List<Emoji>?>
         get() = emojisMutable
 
     init {
-        Singles.zip(
-            mastodonApi.getCustomEmojis(),
-            appDatabase.instanceDao()
-                .loadMetadataForInstance(accountManager.activeAccount?.domain!!)
-                .map<Either<InstanceEntity, Instance>> { Either.Left(it) }
-                .onErrorResumeNext(
-                    mastodonApi.getInstance()
-                        .map { Either.Right(it) }
-                )
-        ) { emojis, either ->
-            either.asLeftOrNull()?.copy(emojiList = emojis)
-                ?: InstanceEntity(
-                    accountManager.activeAccount?.domain!!,
-                    emojis,
-                    either.asRight().maxTootChars,
-                    either.asRight().pollLimits?.maxOptions,
-                    either.asRight().pollLimits?.maxOptionChars,
-                    either.asRight().maxBioChars,
-                    either.asRight().pleroma?.metadata?.fieldsLimits?.maxFields,
-                    either.asRight().version,
-                    either.asRight().chatLimit,
-                    false,
-                    0,
-                    InstanceConstants.DEFAULT_STATUS_MEDIA_SIZE,
-                    InstanceConstants.DEFAULT_STATUS_MEDIA_SIZE,
-                    emptyList()
-                )
+        getEmojis()
+    }
+
+    private fun getEmojis() {
+        job?.cancelIfActive()
+        job = viewModelScope.launch(Dispatchers.IO) {
+            emojisMutable.postValue(instanceRepository.getEmojis())
         }
-            .doOnSuccess {
-                appDatabase.instanceDao().insertOrReplace(it)
-            }
-            .subscribe({ instanceEntity ->
-                emojisMutable.postValue(instanceEntity.emojiList)
-            }, {
-                Log.w(TAG, "Failed to get custom emojis.", it)
-            })
-            .autoDispose()
     }
 
     fun load() {
@@ -103,7 +80,7 @@ class AnnouncementsViewModel(
                                     eventHub.dispatch(AnnouncementReadEvent(announcement.id))
                                 },
                                 { throwable ->
-                                    Log.d(TAG, "Failed to mark announcement as read.", throwable)
+                                    Timber.e(throwable, "Failed to mark announcement as read.")
                                 }
                             )
                             .autoDispose()
@@ -136,15 +113,16 @@ class AnnouncementsViewModel(
                                     } else {
                                         listOf(
                                             *announcement.reactions.toTypedArray(),
-                                            emojis.value!!.find { emoji -> emoji.shortcode == name }!!.run {
-                                                Announcement.Reaction(
-                                                    name,
-                                                    1,
-                                                    true,
-                                                    url,
-                                                    staticUrl
-                                                )
-                                            }
+                                            emojis.value!!.find { emoji -> emoji.shortcode == name }!!
+                                                .run {
+                                                    Announcement.Reaction(
+                                                        name,
+                                                        1,
+                                                        true,
+                                                        url,
+                                                        staticUrl
+                                                    )
+                                                }
                                         )
                                     }
                                 )
@@ -155,7 +133,7 @@ class AnnouncementsViewModel(
                     )
                 )
             }, {
-                Log.w(TAG, "Failed to add reaction to the announcement.", it)
+                Timber.w("Failed to add reaction to the announcement.", it)
             })
             .autoDispose()
     }
@@ -190,12 +168,8 @@ class AnnouncementsViewModel(
                     )
                 )
             }, {
-                Log.w(TAG, "Failed to remove reaction from the announcement.", it)
+                Timber.w("Failed to remove reaction from the announcement.", it)
             })
             .autoDispose()
-    }
-
-    companion object {
-        private const val TAG = "AnnouncementsViewModel"
     }
 }
