d05f8df996b20af29d18494f2bda1881e394d554
Adolfo Santiago <epoch@adol.pw>
2025-05-25 11:02:06 +0200

    Testing reactions on a single status

---
+++ husky/app/src/main/java/com/keylesspalace/tusky/viewmodel/SplashViewModel.kt
@@ -0,0 +1,35 @@
+package com.keylesspalace.tusky.viewmodel
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.keylesspalace.tusky.components.instance.domain.repository.InstanceRepository
+import com.keylesspalace.tusky.db.AccountManager
+import com.keylesspalace.tusky.viewmodel.viewstate.SplashViewState
+import com.keylesspalace.tusky.viewmodel.viewstate.SplashViewState.NO_USER
+import com.keylesspalace.tusky.viewmodel.viewstate.SplashViewState.USER
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+
+class SplashViewModel(
+    val accountManager: AccountManager, private val instanceRepository: InstanceRepository
+) : ViewModel() {
+
+    private val _splashViewState: MutableStateFlow<SplashViewState> =
+        MutableStateFlow(SplashViewState.DEFAULT)
+    val splashViewState = _splashViewState
+
+    init {
+        loadInstanceSettings()
+    }
+
+    private fun loadInstanceSettings() {
+        viewModelScope.launch {
+            instanceRepository.getInstanceInfo().collect {
+                _splashViewState.update {
+                    accountManager.activeAccount?.let { USER } ?: NO_USER
+                }
+            }
+        }
+    }
+}
